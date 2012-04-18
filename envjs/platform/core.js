8a9eb4eb397a4094dea7bddfa417872688fbe75c
Chris Nash <guestblogme@gmail.com>
2012-04-18 15:25:31 -0700

    Incorporated filesystem path fixes for Windows file paths (as in pull request #23, 031b304/b645a70 from moschel. Added Windows batch files to run envjs using either Rhino or the Rhino debugger. Fixed an issue in xhr.js Envjs.localXHR when running under Rhino, url parameter was tested for pattern match to determine Content-Type, but fails under Rhino since the url parameter passed is a Java object, not a string.

--- envjs/platform/core.js
+++ envjs/platform/core.js
@@ -2763,6 +2763,7 @@ Envjs.getcwd = function() {
  * @param {Object} base  (semi-optional)  The base url used in resolving "path" above
  */
 Envjs.uri = function(path, base) {
+	path = path.replace(/\\/g, '/');
     //console.log('constructing uri from path %s and base %s', path, base);
     path = path+'';
     // Semi-common trick is to make an iframe with src='javascript:false'
@@ -2775,6 +2776,12 @@ Envjs.uri = function(path, base) {
     // if path is absolute, then just normalize and return
     if (path.match('^[a-zA-Z]+://')) {
         return urlparse.urlnormalize(path);
+    }
+
+    // if path is a Windows style absolute path (C:\foo\bar\index.html)
+	// make it a file: URL
+    if (path.match('^[a-zA-Z]+:/')) {
+        return 'file:///' + urlparse.urlnormalize(path);
     }
 
     // interesting special case, a few very large websites use
@@ -2798,7 +2805,7 @@ Envjs.uri = function(path, base) {
     // if base is still empty, then we are in QA mode loading local
     // files.  Get current working directory
     if (!base) {
-        base = 'file://' +  Envjs.getcwd() + '/';
+        base = 'file:///' + (""+Envjs.getcwd()).replace(/\\/g, '/') + '/';
     }
     // handles all cases if path is abosulte or relative to base
     // 3rd arg is "false" --> remove fragments
@@ -2884,13 +2891,15 @@ Envjs.localXHR = function(url, xhr, connection, data){
             xhr.statusText = "ok";
             xhr.responseText = Envjs.readFromFile(url);
             try{
-                if(url.match(/html$/)){
+                //url as passed in here might be an object, so stringify it
+                var urlstring = url.toString();
+                if(urlstring.match(/html$/)){
                     xhr.responseHeaders["Content-Type"] = 'text/html';
-                }else if(url.match(/.xml$/)){
+                }else if(urlstring.match(/.xml$/)){
                     xhr.responseHeaders["Content-Type"] = 'text/xml';
-                }else if(url.match(/.js$/)){
+                }else if(urlstring.match(/.js$/)){
                     xhr.responseHeaders["Content-Type"] = 'text/javascript';
-                }else if(url.match(/.json$/)){
+                }else if(urlstring.match(/.json$/)){
                     xhr.responseHeaders["Content-Type"] = 'application/json';
                 }else{
                     xhr.responseHeaders["Content-Type"] = 'text/plain';
@@ -2914,6 +2923,7 @@ Envjs.localXHR = function(url, xhr, connection, data){
 __extend__(Envjs, urlparse);
 
 }(/*Envjs.XMLHttpRequest.Core*/));
+
 (function(){
     
 var log = Envjs.logger('Envjs.Window');
